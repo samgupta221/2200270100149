90f8999da7841383dd7780b9e3c53796d413643f
[Samridhi Gupta] <samridhigupta1944@gmail.com>
2025-07-15 12:38:56 +0530

    Final updation

--- Backend Test Submission/server.js
+++ Backend Test Submission/server.js
@@ -1,38 +1,19 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-// Routes
-const shorturlRoutes = require("./routes/shorturl");
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const logger = require('../log-middleware/logger');
-
-const userRoutes = require('./routes/userRoutes');
-const jobRoutes = require('./routes/jobRoutes');
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(logger); // Custom logger middleware
 
-app.use('/api/users', userRoutes);
-app.use('/api/jobs', jobRoutes);
+app.use(cors());              
+app.use(express.json());     
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
-    });
-  })
-  .catch(err => console.log(err));
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
